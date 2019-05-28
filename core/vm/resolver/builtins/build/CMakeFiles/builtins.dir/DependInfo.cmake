f61cb86e6343e06f7a362c1c18a45a56a4d73105
yangchuang <1715222986@qq.com>
2019-05-28 13:52:30 +0800

    fix compile bug

---
+++ core/vm/resolver/builtins/build/CMakeFiles/builtins.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/qydev/go/src/github.com/dipperin/dipperin-core/core/vm/resolver/builtins/ashriti3.c" "/home/qydev/go/src/github.com/dipperin/dipperin-core/core/vm/resolver/builtins/build/CMakeFiles/builtins.dir/ashriti3.o"
+  "/home/qydev/go/src/github.com/dipperin/dipperin-core/core/vm/resolver/builtins/divti3.c" "/home/qydev/go/src/github.com/dipperin/dipperin-core/core/vm/resolver/builtins/build/CMakeFiles/builtins.dir/divti3.o"
+  "/home/qydev/go/src/github.com/dipperin/dipperin-core/core/vm/resolver/builtins/fixdfti.c" "/home/qydev/go/src/github.com/dipperin/dipperin-core/core/vm/resolver/builtins/build/CMakeFiles/builtins.dir/fixdfti.o"
+  "/home/qydev/go/src/github.com/dipperin/dipperin-core/core/vm/resolver/builtins/fixsfti.c" "/home/qydev/go/src/github.com/dipperin/dipperin-core/core/vm/resolver/builtins/build/CMakeFiles/builtins.dir/fixsfti.o"
+  "/home/qydev/go/src/github.com/dipperin/dipperin-core/core/vm/resolver/builtins/fixtfti.c" "/home/qydev/go/src/github.com/dipperin/dipperin-core/core/vm/resolver/builtins/build/CMakeFiles/builtins.dir/fixtfti.o"
+  "/home/qydev/go/src/github.com/dipperin/dipperin-core/core/vm/resolver/builtins/fixunsdfti.c" "/home/qydev/go/src/github.com/dipperin/dipperin-core/core/vm/resolver/builtins/build/CMakeFiles/builtins.dir/fixunsdfti.o"
+  "/home/qydev/go/src/github.com/dipperin/dipperin-core/core/vm/resolver/builtins/fixunssfti.c" "/home/qydev/go/src/github.com/dipperin/dipperin-core/core/vm/resolver/builtins/build/CMakeFiles/builtins.dir/fixunssfti.o"
+  "/home/qydev/go/src/github.com/dipperin/dipperin-core/core/vm/resolver/builtins/fixunstfti.c" "/home/qydev/go/src/github.com/dipperin/dipperin-core/core/vm/resolver/builtins/build/CMakeFiles/builtins.dir/fixunstfti.o"
+  "/home/qydev/go/src/github.com/dipperin/dipperin-core/core/vm/resolver/builtins/floattidf.c" "/home/qydev/go/src/github.com/dipperin/dipperin-core/core/vm/resolver/builtins/build/CMakeFiles/builtins.dir/floattidf.o"
+  "/home/qydev/go/src/github.com/dipperin/dipperin-core/core/vm/resolver/builtins/floatuntidf.c" "/home/qydev/go/src/github.com/dipperin/dipperin-core/core/vm/resolver/builtins/build/CMakeFiles/builtins.dir/floatuntidf.o"
+  "/home/qydev/go/src/github.com/dipperin/dipperin-core/core/vm/resolver/builtins/modti3.c" "/home/qydev/go/src/github.com/dipperin/dipperin-core/core/vm/resolver/builtins/build/CMakeFiles/builtins.dir/modti3.o"
+  "/home/qydev/go/src/github.com/dipperin/dipperin-core/core/vm/resolver/builtins/multi3.c" "/home/qydev/go/src/github.com/dipperin/dipperin-core/core/vm/resolver/builtins/build/CMakeFiles/builtins.dir/multi3.o"
+  "/home/qydev/go/src/github.com/dipperin/dipperin-core/core/vm/resolver/builtins/udivti3.c" "/home/qydev/go/src/github.com/dipperin/dipperin-core/core/vm/resolver/builtins/build/CMakeFiles/builtins.dir/udivti3.o"
+  "/home/qydev/go/src/github.com/dipperin/dipperin-core/core/vm/resolver/builtins/umodti3.c" "/home/qydev/go/src/github.com/dipperin/dipperin-core/core/vm/resolver/builtins/build/CMakeFiles/builtins.dir/umodti3.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../"
+  "../../softfloat/source/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
