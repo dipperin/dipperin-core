0cabd6411d5a0ee565db2999d4db9f1bdbcd23b5
nuistzj2014 <nuistzj@163.com>
2020-01-15 15:53:36 +0800

    add chain tests

--- core/chain/block_processor_test.go
+++ core/chain/block_processor_test.go
@@ -40,32 +40,79 @@ func TestNewBlockProcessor(t *testing.T) {
 }
 
 func TestBlockProcessor_Process(t *testing.T) {
-	db, root := createTestStateDB(t)
-	tdb := stateprocessor.NewStateStorageWithCache(db)
-	state, err := stateprocessor.NewAccountStateDB(root, tdb)
-	assert.NoError(t, err)
-	processor, err := NewBlockProcessor(fakeAccountDBChain{state: state}, root, tdb)
-	assert.NoError(t, err)
+	type result struct {
+		err error
+	}
 
-	// block.Number() == 0 return
-	block := model.CreateBlock(0, common.Hash{}, 0)
-	err = processor.Process(block, fakeEconomyModel{})
-	assert.NoError(t, err)
+	testCases := []struct {
+		name   string
+		given  func() error
+		expect result
+	}{
+		{
+			name:"NewAccountStateDB and NewBlockProcessor",
+			given: func() error {
+				db, root := createTestStateDB(t)
+				tdb := stateprocessor.NewStateStorageWithCache(db)
+				state, err := stateprocessor.NewAccountStateDB(root, tdb)
+				_, err = NewBlockProcessor(fakeAccountDBChain{state: state}, root, tdb)
+				return err
+			},
+			expect:result{nil},
+		},
+		{
+			name:"return block number is zero",
+			given: func() error {
+				db, root := createTestStateDB(t)
+				tdb := stateprocessor.NewStateStorageWithCache(db)
+				state, _ := stateprocessor.NewAccountStateDB(root, tdb)
+				processor, err := NewBlockProcessor(fakeAccountDBChain{state: state}, root, tdb)
+				block := model.CreateBlock(0, common.Hash{}, 0)
+				err = processor.Process(block, fakeEconomyModel{})
+				return err
+			},
+			expect:result{nil},
+		},
+		{
+			name:"block number 9 isn't change point",
+			given: func() error {
+				db, root := createTestStateDB(t)
+				tdb := stateprocessor.NewStateStorageWithCache(db)
+				state, _ := stateprocessor.NewAccountStateDB(root, tdb)
+				processor, err := NewBlockProcessor(fakeAccountDBChain{state: state}, root, tdb)
+				block := createBlock(10)
+				err = processor.Process(block, fakeEconomyModel{})
+				return err
+			},
+			expect:result{gerror.ErrAccountNotExist},
+		},
+		{
+			name:"block number 19 is change point",
+			given: func() error {
+				db, root := createTestStateDB(t)
+				tdb := stateprocessor.NewStateStorageWithCache(db)
+				state, _ := stateprocessor.NewAccountStateDB(root, tdb)
+				processor, err := NewBlockProcessor(fakeAccountDBChain{state: state}, root, tdb)
+				block := model.CreateBlock(20, common.Hash{}, 0)
+				err = processor.Process(block, fakeEconomyModel{})
+				return err
+			},
+			expect:result{nil},
+		},
+	}
 
-	// blockNum = 9 isn't change point
-	block = createBlock(10)
-	err = processor.Process(block, fakeEconomyModel{})
-	assert.Equal(t, gerror.ErrAccountNotExist, err)
-
-	// blockNum = 19 is change point
-	block = model.CreateBlock(20, common.Hash{}, 0)
-	err = processor.Process(block, fakeEconomyModel{})
-	assert.NoError(t, err)
+	for _,tc:=range testCases{
+		err:=tc.given()
+		if err!=nil{
+			assert.Equal(t,tc.expect.err,err)
+		}else {
+			assert.NoError(t,err)
+		}
+	}
 }
 
 func TestBlockProcessor_Process_Error(t *testing.T) {
 	header := model.NewHeader(1, 10, common.Hash{}, common.HexToHash("1111"), minDiff, big.NewInt(324234), common.Address{}, common.BlockNonceFromInt(432423))
-
 	tx := createUnNormalTx()
 	block := model.NewBlock(header, []*model.Transaction{tx}, nil)
 
@@ -74,14 +121,42 @@ func TestBlockProcessor_Process_Error(t *testing.T) {
 	processor, err := NewBlockProcessor(fakeAccountDBChain{}, root, tdb)
 	assert.NoError(t, err)
 
-	// TxIterator inner error
-	err = processor.Process(block, fakeEconomyModel{})
-	assert.Equal(t, gerror.ErrUnknownTxType, err)
+	type result struct {
+		err error
+	}
 
-	// doReward- RewardCoinBase no coin base error
-	block = model.NewBlock(header, nil, nil)
-	err = processor.Process(block, fakeEconomyModel{})
-	assert.Equal(t, gerror.InvalidCoinBaseAddressErr, err)
+	testCases := []struct {
+		name   string
+		given  func() error
+		expect result
+	}{
+		{
+			name:"TxIterator inner error",
+			given: func() error {
+				err = processor.Process(block, fakeEconomyModel{})
+				return err
+			},
+			expect:result{gerror.ErrUnknownTxType},
+		},
+		{
+			name:"doReward- RewardCoinBase no coin base error",
+			given: func() error {
+				block = model.NewBlock(header, nil, nil)
+				err = processor.Process(block, fakeEconomyModel{})
+				return err
+			},
+			expect:result{gerror.InvalidCoinBaseAddressErr},
+		},
+	}
+
+	for _,tc:=range testCases{
+		err:=tc.given()
+		if err!=nil{
+			assert.Equal(t,tc.expect.err,err)
+		}else {
+			assert.NoError(t,err)
+		}
+	}
 }
 
 func TestBlockProcessor_processorCommitList_Error(t *testing.T) {
@@ -90,21 +165,55 @@ func TestBlockProcessor_processorCommitList_Error(t *testing.T) {
 	processor, err := NewBlockProcessor(fakeAccountDBChain{}, common.Hash{}, tdb)
 	assert.NoError(t, err)
 
-	block := createBlock(1)
-	err = processor.processCommitList(block, false)
-	assert.Equal(t, gerror.NotHavePreBlockErr, err)
-
-	block = createBlock(20)
-	err = processor.processCommitList(block, false)
-	assert.Equal(t, gerror.ErrAccountNotExist, err)
-
-	for i := 0; i < len(VerifierAddress); i++ {
-		err = processor.NewAccountState(VerifierAddress[i])
-		assert.NoError(t, err)
+	type result struct {
+		err error
 	}
 
-	err = processor.processCommitList(block, false)
-	assert.Equal(t, gerror.ErrAccountNotExist, err)
+	testCases := []struct {
+		name   string
+		given  func() error
+		expect result
+	}{
+		{
+			name:"NotHavePreBlock Error",
+			given: func() error {
+				block := createBlock(1)
+				err = processor.processCommitList(block, false)
+				return err
+			},
+			expect:result{gerror.NotHavePreBlockErr},
+		},
+		{
+			name:"Account Not Exist",
+			given: func() error {
+				block := createBlock(20)
+				err = processor.processCommitList(block, false)
+				return err
+			},
+			expect:result{gerror.ErrAccountNotExist},
+		},
+		{
+			name:"NewAccountState AccountNotExist",
+			given: func() error {
+				block := createBlock(20)
+				for i := 0; i < len(VerifierAddress); i++ {
+					err = processor.NewAccountState(VerifierAddress[i])
+				}
+				err = processor.processCommitList(block, false)
+				return err
+			},
+			expect:result{gerror.ErrAccountNotExist},
+		},
+	}
+
+	for _,tc:=range testCases {
+		err:=tc.given()
+		if err!=nil{
+			assert.Equal(t,tc.expect.err,err)
+		}else {
+			assert.NoError(t,err)
+		}
+	}
 }
 
 func TestBlockProcessor_doReward_Error(t *testing.T) {
@@ -125,4 +234,3 @@ func TestBlockProcessor_doReward_Error(t *testing.T) {
 	err = processor.doRewards(block)
 	assert.Equal(t, gerror.NotHavePreBlockErr, err)
 }
-
