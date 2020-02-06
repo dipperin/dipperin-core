655622e6090ea1b84789af89f7ef82d1835acac9
Min65535 <22154915+Min65535@users.noreply.github.com>
2020-02-06 14:32:30 +0800

    fix test codes

--- core/csbft/components/fetcher_test.go
+++ core/csbft/components/fetcher_test.go
@@ -21,36 +21,47 @@ func TestCsBftFetcher_FetchBlock(t *testing.T) {
 	gomock.InOrder(
 		fetcher.EXPECT().FetchBlock(ww, common.HexToHash(hashTmp)).Return(nil).AnyTimes(),
 	)
-	//NewFetcher(nil).FetchBlock(ww, common.HexToHash(hashTmp))
 }
 
 func TestCsBftFetcher_FetchBlockResp(t *testing.T) {
 	rsp := NewFetcher(nil)
-	rsp.FetchBlockResp(nil)
+	assert.NotEmpty(t, rsp)
+	assert.NotPanics(t, func() {
+		rsp.FetchBlockResp(nil)
+	})
 }
 
-func TestCsBftFetcher_rmReq(t *testing.T)  {
+func TestCsBftFetcher_rmReq(t *testing.T) {
 	rsp := NewFetcher(nil)
-	rsp.rmReq(1)
+	assert.NotEmpty(t, rsp)
+	assert.NotPanics(t, func() {
+		rsp.rmReq(1)
+	})
 }
 
 func TestCsBftFetcher_OnStart(t *testing.T) {
 	rsp := NewFetcher(nil)
+	assert.NotEmpty(t, rsp)
 	assert.NoError(t, rsp.OnStart())
 }
 
 func TestCsBftFetcher_OnStop(t *testing.T) {
 	rsp := NewFetcher(nil)
-	rsp.OnStop()
+	assert.NotEmpty(t, rsp)
+	assert.NotPanics(t, func() {
+		rsp.OnStop()
+	})
 }
 
 func TestCsBftFetcher_OnReset(t *testing.T) {
 	rsp := NewFetcher(nil)
+	assert.NotEmpty(t, rsp)
 	assert.NoError(t, rsp.OnReset())
 }
 
 func TestCsBftFetcher_IsFetching(t *testing.T) {
 	rsp := NewFetcher(nil)
+	assert.NotEmpty(t, rsp)
 	var hashTmp = `0xd50866a60b4f7e4123400e0563efb987dc800d1a72af5cc1ae9ee68760bb18889`
 	assert.Equal(t, false, rsp.isFetching(common.HexToHash(hashTmp)))
 }
