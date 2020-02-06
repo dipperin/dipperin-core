08618461901519a76a1f34bd398053c90984a7e7
Min65535 <22154915+Min65535@users.noreply.github.com>
2020-02-06 17:20:04 +0800

    add test codes

--- core/csbft/components/fetcher_test.go
+++ core/csbft/components/fetcher_test.go
@@ -5,6 +5,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/golang/mock/gomock"
 	"github.com/dipperin/dipperin-core/common"
+	"github.com/dipperin/dipperin-core/core/model"
 )
 
 func TestNewFetcher(t *testing.T) {
@@ -59,9 +60,67 @@ func TestCsBftFetcher_OnReset(t *testing.T) {
 	assert.NoError(t, rsp.OnReset())
 }
 
+func TestCsBftFetcher_loop(t *testing.T) {
+	rsp := NewFetcher(nil)
+	assert.NotEmpty(t, rsp)
+	assert.NotPanics(t, func() {
+		go func(rsp *CsBftFetcher) {
+			rsp.loop()
+		}(rsp)
+	})
+}
+
 func TestCsBftFetcher_IsFetching(t *testing.T) {
 	rsp := NewFetcher(nil)
 	assert.NotEmpty(t, rsp)
 	var hashTmp = `0xd50866a60b4f7e4123400e0563efb987dc800d1a72af5cc1ae9ee68760bb18889`
+	assert.Equal(t, false, rsp.IsFetching(common.HexToHash(hashTmp)))
+}
+
+func TestCsBftFetcher_isFetching(t *testing.T) {
+	rsp := NewFetcher(nil)
+	assert.NotEmpty(t, rsp)
+	var hashTmp = `0xd50866a60b4f7e4123400e0563efb987dc800d1a72af5cc1ae9ee68760bb18889`
 	assert.Equal(t, false, rsp.isFetching(common.HexToHash(hashTmp)))
+}
+
+func TestCsBftFetcher_onFetchBlock(t *testing.T) {
+	rsp := NewFetcher(nil)
+	assert.NotEmpty(t, rsp)
+	var hashTmp = `0xd50866a60b4f7e4123400e0563efb987dc800d1a72af5cc1ae9ee68760bb18889`
+	var req = FetchBlockReqMsg{
+		MsgId:      0,
+		From:       common.HexToAddress(hashTmp),
+		BlockHash:  common.HexToHash(hashTmp),
+		ResultChan: make(chan model.AbstractBlock),
+	}
+	assert.Panics(t, func() {
+		rsp.onFetchBlock(&req)
+	})
+}
+
+func TestCsBftFetcher_onFetchResp(t *testing.T) {
+	rsp := NewFetcher(nil)
+	assert.NotEmpty(t, rsp)
+	var req = FetchBlockRespMsg{
+		MsgId: 1,
+		Block: nil,
+	}
+	var hashTmp = `0xd50866a60b4f7e4123400e0563efb987dc800d1a72af5cc1ae9ee68760bb18889`
+	r := FetchBlockReqMsg{
+		MsgId:      0,
+		From:       common.HexToAddress(hashTmp),
+		BlockHash:  common.HexToHash(hashTmp),
+		ResultChan: make(chan model.AbstractBlock),
+	}
+	rsp.requests[1] = &r
+	assert.Panics(t, func() {
+		rsp.onFetchResp(&req)
+	})
+
+	rsp1 := NewFetcher(nil)
+	assert.NotEmpty(t, rsp1)
+	assert.NotPanics(t, func() {
+		rsp1.onFetchResp(&req)
+	})
 }
