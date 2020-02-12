dd07dc1aae52d8cda80b9a492c9d929b98a2a1f3
Min65535 <22154915+Min65535@users.noreply.github.com>
2020-02-12 14:06:48 +0800

    add cods

--- core/csbft/components/blockpool_test.go
+++ core/csbft/components/blockpool_test.go
@@ -56,12 +56,9 @@ func TestBlockPool_SetNodeConfig(t *testing.T) {
 	}
 }
 
-type myTestNotifier struct {
-}
+type myTestNotifier struct{}
 
-func (myTestNotifier) BlockPoolNotEmpty() {
-	panic("implement me")
-}
+func (myTestNotifier) BlockPoolNotEmpty() {}
 
 func TestBlockPool_SetPoolEventNotifier(t *testing.T) {
 	testCases := []struct {
@@ -70,7 +67,7 @@ func TestBlockPool_SetPoolEventNotifier(t *testing.T) {
 		expect bool
 	}{
 		{
-			name: "SetNodeConfig true",
+			name: "SetPoolEventNotifier true",
 			given: func() bool {
 				rsp := NewBlockPool(0, nil)
 				assert.NotEmpty(t, rsp)
@@ -80,7 +77,7 @@ func TestBlockPool_SetPoolEventNotifier(t *testing.T) {
 			expect: true,
 		},
 		{
-			name: "SetNodeConfig false",
+			name: "SetPoolEventNotifier false",
 			given: func() bool {
 				rsp := NewBlockPool(0, nil)
 				rsp.SetPoolEventNotifier(myTestNotifier{})
@@ -107,7 +104,7 @@ func TestBlockPool_Start(t *testing.T) {
 		expect string
 	}{
 		{
-			name: "SetNodeConfig false",
+			name: "BlockPool Start false",
 			given: func() string {
 				rsp := &BlockPool{
 					height:            0,
@@ -127,7 +124,7 @@ func TestBlockPool_Start(t *testing.T) {
 		},
 
 		{
-			name: "SetNodeConfig true",
+			name: "BlockPool Start true",
 			given: func() string {
 				rsp := NewBlockPool(0, nil)
 				assert.NotEmpty(t, rsp)
@@ -150,55 +147,354 @@ func TestBlockPool_Start(t *testing.T) {
 }
 
 func TestBlockPool_Stop(t *testing.T) {
-	rsp := NewBlockPool(0, nil)
-	assert.NotEmpty(t, rsp)
-	if assert.NoError(t, rsp.Start()) {
-		time.Sleep(500 * time.Millisecond)
-		rsp.Stop()
+	testCases := []struct {
+		name   string
+		given  func() bool
+		expect bool
+	}{
+		{
+			name: "stopChan is nil",
+			given: func() bool {
+				rsp := NewBlockPool(0, nil)
+				assert.NotEmpty(t, rsp)
+				return assert.NotPanics(t, rsp.Stop)
+			},
+			expect: true,
+		},
+		{
+			name: "stopChan is not nil",
+			given: func() bool {
+				rsp := &BlockPool{
+					height:            0,
+					blocks:            []model.AbstractBlock{},
+					poolEventNotifier: myTestNotifier{},
+					Blockpoolconfig:   nil,
+
+					newHeightChan: make(chan uint64, 5),
+					newBlockChan:  make(chan newBlockWithResultErr, 5),
+					getterChan:    make(chan *blockPoolGetter, 5),
+					rmBlockChan:   make(chan common.Hash),
+					stopChan:      make(chan struct{}),
+				}
+				rsp.Stop()
+				rsp.stopChan = nil
+				return rsp.stopChan == nil
+			},
+			expect: true,
+		},
+	}
+
+	for i, tc := range testCases {
+		sign := tc.given()
+		if testCases[i].expect == sign {
+			t.Log("success")
+		} else {
+			t.Logf("expect:%v,actual:%v", testCases[i].expect, sign)
+		}
 	}
 }
 
 func TestBlockPool_IsEmpty(t *testing.T) {
-	sign := NewBlockPool(0, nil).IsEmpty()
-	assert.Equal(t, sign, true)
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	testCases := []struct {
+		name   string
+		given  func() bool
+		expect bool
+	}{
+		{
+			name: "IsEmpty is false",
+			given: func() bool {
+				rsp := NewBlockPool(0, nil)
+				assert.NotEmpty(t, rsp)
+				rsp.blocks = append(rsp.blocks, NewMockAbstractBlock(ctrl))
+				return rsp.IsEmpty()
+			},
+			expect: false,
+		},
+		{
+			name: "IsEmpty is true",
+			given: func() bool {
+				rsp := NewBlockPool(0, nil)
+				assert.NotEmpty(t, rsp)
+				return rsp.IsEmpty()
+			},
+			expect: true,
+		},
+	}
+
+	for i, tc := range testCases {
+		sign := tc.given()
+		if testCases[i].expect == sign {
+			t.Log("success")
+		} else {
+			t.Logf("expect:%v,actual:%v", testCases[i].expect, sign)
+		}
+	}
 }
 
 func TestBlockPool_loop(t *testing.T) {
-	rsp := NewBlockPool(0, nil)
-	assert.NotEmpty(t, rsp)
-	go rsp.loop()
-	go func(rsp *BlockPool) {
-		rsp.stopChan = make(chan struct{})
-		rsp.stopChan <- struct{}{}
-	}(rsp)
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	testCases := []struct {
+		name   string
+		given  func() bool
+		expect bool
+	}{
+		{
+			name: "IsEmpty is false",
+			given: func() bool {
+				rsp := &BlockPool{
+					height:            0,
+					blocks:            []model.AbstractBlock{},
+					poolEventNotifier: myTestNotifier{},
+					Blockpoolconfig:   nil,
+
+					newHeightChan: make(chan uint64, 5),
+					newBlockChan:  make(chan newBlockWithResultErr, 5),
+					getterChan:    make(chan *blockPoolGetter, 5),
+					rmBlockChan:   make(chan common.Hash),
+					stopChan:      make(chan struct{}),
+				}
+				//go rsp.loop()
+				go func(rsp *BlockPool) {
+					time.Sleep(time.Second)
+					rsp.NewHeight(1)
+					b := NewMockAbstractBlock(ctrl)
+					b.EXPECT().Number().Return(uint64(1)).AnyTimes()
+					b.EXPECT().Hash().Return(common.Hash{}).AnyTimes()
+					rsp.AddBlock(b)
+					resultC := make(chan model.AbstractBlock)
+					rsp.getBlock(&blockPoolGetter{
+						blockHash:  common.Hash{},
+						resultChan: resultC,
+					})
+
+					rsp.RemoveBlock(common.Hash{})
+					rsp.stopChan <- struct{}{}
+				}(rsp)
+				return assert.NotPanics(t, func() {
+					time.Sleep(2 * time.Second)
+					go rsp.loop()
+				})
+			},
+			expect: true,
+		},
+	}
+
+	for i, tc := range testCases {
+		sign := tc.given()
+		if testCases[i].expect == sign {
+			t.Log("success")
+		} else {
+			t.Logf("expect:%v,actual:%v", testCases[i].expect, sign)
+		}
+	}
 }
 
 func TestBlockPool_doRemoveBlock(t *testing.T) {
-	rsp := NewBlockPool(0, nil)
-	assert.NotEmpty(t, rsp)
-	var hashTmp = `0xd50866a60b4f7e4123400e0563efb987dc800d1a72af5cc1ae9ee68760bb18889`
-	rsp.doRemoveBlock(common.HexToHash(hashTmp))
-	assert.Equal(t, 0, len(rsp.blocks))
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	testCases := []struct {
+		name   string
+		given  func() bool
+		expect bool
+	}{
+		{
+			name: "doRemoveBlock test true",
+			given: func() bool {
+				rsp := NewBlockPool(0, nil)
+				assert.NotEmpty(t, rsp)
+				var hashTmp = `0xd50866a60b4f7e494400e0563efb987dc800d1a72af5cc1ae9ee68760bb18889`
+				b := NewMockAbstractBlock(ctrl)
+				b.EXPECT().Hash().Return(common.HexToHash(hashTmp)).AnyTimes()
+				rsp.blocks = append(rsp.blocks, b)
+				rsp.doRemoveBlock(common.HexToHash(hashTmp))
+				return len(rsp.blocks) == 0
+			},
+			expect: true,
+		},
+		{
+			name: "doRemoveBlock test false",
+			given: func() bool {
+				rsp := NewBlockPool(0, nil)
+				assert.NotEmpty(t, rsp)
+				var hashTmp1 = `0xd50866a60b4f7e494400e0563efb987dc800d1a72af5cc1ae9ee68760bb18889`
+				var hashTmp2 = `0xd51866a60b4f7e494400e0563efb987dc800d1a72af5cc1ae9ee68760bb18889`
+				b := NewMockAbstractBlock(ctrl)
+				b.EXPECT().Hash().Return(common.HexToHash(hashTmp1)).AnyTimes()
+				rsp.blocks = append(rsp.blocks, b)
+				rsp.doRemoveBlock(common.HexToHash(hashTmp2))
+				return len(rsp.blocks) == 0
+			},
+			expect: false,
+		},
+	}
+
+	for i, tc := range testCases {
+		sign := tc.given()
+		if testCases[i].expect == sign {
+			t.Log("success")
+		} else {
+			t.Logf("expect:%v,actual:%v", testCases[i].expect, sign)
+		}
+	}
 }
 
 func TestBlockPool_IsRunning(t *testing.T) {
-	sign := NewBlockPool(0, nil).IsRunning()
-	assert.Equal(t, sign, false)
+	testCases := []struct {
+		name   string
+		given  func() bool
+		expect bool
+	}{
+		{
+			name: "IsRunning false",
+			given: func() bool {
+				rsp := NewBlockPool(0, nil)
+				assert.NotEmpty(t, rsp)
+				return rsp.IsRunning()
+			},
+			expect: false,
+		},
+		{
+			name: "IsRunning true",
+			given: func() bool {
+				rsp := &BlockPool{
+					height:            0,
+					blocks:            []model.AbstractBlock{},
+					poolEventNotifier: myTestNotifier{},
+					Blockpoolconfig:   nil,
+
+					newHeightChan: make(chan uint64, 5),
+					newBlockChan:  make(chan newBlockWithResultErr, 5),
+					getterChan:    make(chan *blockPoolGetter, 5),
+					rmBlockChan:   make(chan common.Hash),
+					stopChan:      make(chan struct{}),
+				}
+				return rsp.IsRunning()
+			},
+			expect: true,
+		},
+	}
+
+	for i, tc := range testCases {
+		sign := tc.given()
+		if testCases[i].expect == sign {
+			t.Log("success")
+		} else {
+			t.Logf("expect:%v,actual:%v", testCases[i].expect, sign)
+		}
+	}
 }
 
 func TestBlockPool_RemoveBlock(t *testing.T) {
-	rsp := NewBlockPool(0, nil)
-	assert.NotEmpty(t, rsp)
-	var hashTmp = `0xd50866a60b4f7e4123400e0563efb987dc800d1a72af5cc1ae9ee68760bb18889`
-	rsp.RemoveBlock(common.HexToHash(hashTmp))
-	assert.Equal(t, 0, len(rsp.rmBlockChan))
+	testCases := []struct {
+		name   string
+		given  func() bool
+		expect bool
+	}{
+		{
+			name: "RemoveBlock true",
+			given: func() bool {
+				rsp := &BlockPool{
+					height:            0,
+					blocks:            []model.AbstractBlock{},
+					poolEventNotifier: myTestNotifier{},
+					Blockpoolconfig:   nil,
+
+					newHeightChan: make(chan uint64, 5),
+					newBlockChan:  make(chan newBlockWithResultErr, 5),
+					getterChan:    make(chan *blockPoolGetter, 5),
+					rmBlockChan:   make(chan common.Hash),
+					stopChan:      make(chan struct{}),
+				}
+				var hashTmp = `0xd50866a60b4f7e494400e0563efb987dc800d1a72af5cc1ae9ee68760bb18889`
+				assert.NotPanics(t, func() {
+					go func(*BlockPool) {
+						rsp.RemoveBlock(common.HexToHash(hashTmp))
+					}(rsp)
+				})
+				r := <-rsp.rmBlockChan
+				//close(rsp.rmBlockChan)
+				//rsp.rmBlockChan = nil
+				t.Log("r:", r.Hex())
+				return r.Hex() != ""
+			},
+			expect: true,
+		},
+		{
+			name: "RemoveBlock false",
+			given: func() bool {
+				rsp := NewBlockPool(0, nil)
+				assert.NotEmpty(t, rsp)
+				rsp.RemoveBlock(common.Hash{})
+				return len(rsp.rmBlockChan) > 0
+			},
+			expect: false,
+		},
+	}
+
+	for i, tc := range testCases {
+		sign := tc.given()
+		if testCases[i].expect == sign {
+			t.Log("success")
+		} else {
+			t.Logf("expect:%v,actual:%v", testCases[i].expect, sign)
+		}
+	}
 }
 
 func TestBlockPool_NewHeight(t *testing.T) {
-	rsp := NewBlockPool(0, nil)
-	assert.NotEmpty(t, rsp)
-	rsp.NewHeight(0)
-	assert.Equal(t, 0, len(rsp.newHeightChan))
+	testCases := []struct {
+		name   string
+		given  func() bool
+		expect bool
+	}{
+		{
+			name: "NewHeight true",
+			given: func() bool {
+				rsp := &BlockPool{
+					height:            0,
+					blocks:            []model.AbstractBlock{},
+					poolEventNotifier: myTestNotifier{},
+					Blockpoolconfig:   nil,
+
+					newHeightChan: make(chan uint64, 5),
+					newBlockChan:  make(chan newBlockWithResultErr, 5),
+					getterChan:    make(chan *blockPoolGetter, 5),
+					rmBlockChan:   make(chan common.Hash),
+					stopChan:      make(chan struct{}),
+				}
+				assert.NotPanics(t, func() {
+					go func(*BlockPool) {
+						rsp.NewHeight(1)
+					}(rsp)
+				})
+				r := <-rsp.newHeightChan
+				return r != 0
+			},
+			expect: true,
+		},
+		{
+			name: "NewHeight false",
+			given: func() bool {
+				rsp := NewBlockPool(0, nil)
+				assert.NotEmpty(t, rsp)
+				rsp.NewHeight(0)
+				return len(rsp.newHeightChan) > 0
+			},
+			expect: false,
+		},
+	}
+
+	for i, tc := range testCases {
+		sign := tc.given()
+		if testCases[i].expect == sign {
+			t.Log("success")
+		} else {
+			t.Logf("expect:%v,actual:%v", testCases[i].expect, sign)
+		}
+	}
 }
 
 func TestBlockPool_doNewHeight(t *testing.T) {
